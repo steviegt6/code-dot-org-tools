8cbbd7e3ed4dfc62d4635a3a24f4fd2d46c3e2e2
Tomat <xxlennygamerxx@gmail.com>
2021-09-01 11:07:30 -0700

    Create utilities.js

---
+++ Turtle/utilities.js
@@ -0,0 +1,15 @@
+/* These utilities make use of turtle methods with parameters.
+ * See utilities-parameterless.js for parameterless utilities.
+ * These classes do not contain alternatives for eachother.
+ */
+
+/*
+ * Turns a turtle 180 degrees.
+ */
+function turnAround() {
+  turnLeft(180);
+  
+  // Parameterless alternative:
+  // turnLeft();
+  // turnLeft();
+}
